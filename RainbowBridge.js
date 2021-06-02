7703595d2cc00b8223e9fa0276caef4c8d2a7166
123yandaixin <53696383+123yandaixin@users.noreply.github.com>
2021-06-02 18:05:20 +0800

    Update RainbowBridge.js

--- RainbowBridge.js
+++ RainbowBridge.js
@@ -13,23 +13,23 @@
  params是一串json字符串
  */
 (function () {
-    var doc = document;
-    var win = window;
-    var ua = win.navigator.userAgent;
-    var JS_BRIDGE_PROTOCOL_SCHEMA = "rainbow";
-    var increase = 1;
+//     var doc = document;
+//     var win = window;
+//     var ua = win.navigator.userAgent;
+//     var JS_BRIDGE_PROTOCOL_SCHEMA = "rainbow";
+//     var increase = 1;
     var RainbowBridge = win.RainbowBridge || (win.RainbowBridge = {});
 
     var ExposeMethod = {
 
-        callMethod: function (clazz, method, param, callback) {
-            var port = PrivateMethod.generatePort();
-            if (typeof callback !== 'function') {
-                callback = null;
-            }
-            PrivateMethod.registerCallback(port, callback);
-            PrivateMethod.callNativeMethod(clazz, port, method, param);
-        },
+//         callMethod: function (clazz, method, param, callback) {
+//             var port = PrivateMethod.generatePort();
+//             if (typeof callback !== 'function') {
+//                 callback = null;
+//             }
+//             PrivateMethod.registerCallback(port, callback);
+//             PrivateMethod.callNativeMethod(clazz, port, method, param);
+//         },
 
         onComplete: function (port, result) {
             // PrivateMethod.onNativeComplete(port, result);
@@ -39,81 +39,81 @@
 
     };
 
-    var PrivateMethod = {
-        callbacks: {},
-        registerCallback: function (port, callback) {
-            if (callback) {
-                PrivateMethod.callbacks[port] = callback;
-            }
-        },
-        getCallback: function (port) {
-            var call = {};
-            if (PrivateMethod.callbacks[port]) {
-                call.callback = PrivateMethod.callbacks[port];
-            } else {
-                call.callback = null;
-            }
-            return call;
-        },
-        unRegisterCallback: function (port) {
-            if (PrivateMethod.callbacks[port]) {
-                delete PrivateMethod.callbacks[port];
-            }
-        },
-        onNativeComplete: function (port, result) {
-            var resultJson = PrivateMethod.str2Json(result);
-            var callback = PrivateMethod.getCallback(port).callback;
-            PrivateMethod.unRegisterCallback(port);
-            if (callback) {
-                //执行回调
-                callback && callback(resultJson);
-            }
-        },
-        generatePort: function () {
-            return Math.floor(Math.random() * (1 << 50)) + '' + increase++;
-        },
-        str2Json: function (str) {
-            if (str && typeof str === 'string') {
-                try {
-                    return JSON.parse(str);
-                } catch (e) {
-                    return {
-                        status: {
-                            code: 1,
-                            msg: 'params parse error!'
-                        }
-                    };
-                }
-            } else {
-                return str || {};
-            }
-        },
-        json2Str: function (param) {
-            if (param && typeof param === 'object') {
-                return JSON.stringify(param);
-            } else {
-                return param || '';
-            }
-        },
-        callNativeMethod: function (clazz, port, method, param) {
-            if (PrivateMethod.isAndroid()) {
-                var jsonStr = PrivateMethod.json2Str(param);
-                var uri = JS_BRIDGE_PROTOCOL_SCHEMA + "://" + clazz + ":" + port + "/" + method + "?" + jsonStr;
-                //win.prompt(uri, "");
-				win.android.callCommonMethod(uri);
-            }
-        },
-        isAndroid: function () {
-            var tmp = ua.toLowerCase();
-            var android = tmp.indexOf("android") > -1;
-            return !!android;
-        },
-        isIos: function () {
-            var tmp = ua.toLowerCase();
-            var ios = tmp.indexOf("iphone") > -1;
-            return !!ios;
-        }
-    };
+//     var PrivateMethod = {
+//         callbacks: {},
+//         registerCallback: function (port, callback) {
+//             if (callback) {
+//                 PrivateMethod.callbacks[port] = callback;
+//             }
+//         },
+//         getCallback: function (port) {
+//             var call = {};
+//             if (PrivateMethod.callbacks[port]) {
+//                 call.callback = PrivateMethod.callbacks[port];
+//             } else {
+//                 call.callback = null;
+//             }
+//             return call;
+//         },
+//         unRegisterCallback: function (port) {
+//             if (PrivateMethod.callbacks[port]) {
+//                 delete PrivateMethod.callbacks[port];
+//             }
+//         },
+//         onNativeComplete: function (port, result) {
+//             var resultJson = PrivateMethod.str2Json(result);
+//             var callback = PrivateMethod.getCallback(port).callback;
+//             PrivateMethod.unRegisterCallback(port);
+//             if (callback) {
+//                 //执行回调
+//                 callback && callback(resultJson);
+//             }
+//         },
+//         generatePort: function () {
+//             return Math.floor(Math.random() * (1 << 50)) + '' + increase++;
+//         },
+//         str2Json: function (str) {
+//             if (str && typeof str === 'string') {
+//                 try {
+//                     return JSON.parse(str);
+//                 } catch (e) {
+//                     return {
+//                         status: {
+//                             code: 1,
+//                             msg: 'params parse error!'
+//                         }
+//                     };
+//                 }
+//             } else {
+//                 return str || {};
+//             }
+//         },
+//         json2Str: function (param) {
+//             if (param && typeof param === 'object') {
+//                 return JSON.stringify(param);
+//             } else {
+//                 return param || '';
+//             }
+//         },
+//         callNativeMethod: function (clazz, port, method, param) {
+//             if (PrivateMethod.isAndroid()) {
+//                 var jsonStr = PrivateMethod.json2Str(param);
+//                 var uri = JS_BRIDGE_PROTOCOL_SCHEMA + "://" + clazz + ":" + port + "/" + method + "?" + jsonStr;
+//                 //win.prompt(uri, "");
+// 				win.android.callCommonMethod(uri);
+//             }
+//         },
+//         isAndroid: function () {
+//             var tmp = ua.toLowerCase();
+//             var android = tmp.indexOf("android") > -1;
+//             return !!android;
+//         },
+//         isIos: function () {
+//             var tmp = ua.toLowerCase();
+//             var ios = tmp.indexOf("iphone") > -1;
+//             return !!ios;
+//         }
+//     };
     for (var index in ExposeMethod) {
         if (ExposeMethod.hasOwnProperty(index)) {
             if (!Object.prototype.hasOwnProperty.call(RainbowBridge, index)) {
