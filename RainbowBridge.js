2f061df35a3b003eefa4789ce3d26e1e81cac4c0
123yandaixin <53696383+123yandaixin@users.noreply.github.com>
2021-06-02 17:53:46 +0800

    Update RainbowBridge.js

--- RainbowBridge.js
+++ RainbowBridge.js
@@ -1,20 +1,36 @@
-// function onComplete(msg1,msg2){
+(function () {
+//     var doc = document;
+//     var win = window;
+//     var ua = win.navigator.userAgent;
+//     var JS_BRIDGE_PROTOCOL_SCHEMA = "rainbow";
+//     var increase = 1;
+//     var RainbowBridge = win.RainbowBridge || (win.RainbowBridge = {});
 
-//     var jsonstr=JSON.stringify(msg2);
-//     alert(jsonstr);
-    
-// }
+    var ExposeMethod = {
 
-(function(){
+//         callMethod: function (clazz, method, param, callback) {
+//             var port = PrivateMethod.generatePort();
+//             if (typeof callback !== 'function') {
+//                 callback = null;
+//             }
+//             PrivateMethod.registerCallback(port, callback);
+//             PrivateMethod.callNativeMethod(clazz, port, method, param);
+//         },
 
-  var abc={
+        onComplete: function (port, result) {
+            // PrivateMethod.onNativeComplete(port, result);
+            var jsonStr=JSON.stringify(result);
+            alert(jsonStr);
+        }
 
-    onComplete: function (port, result) {
-        // PrivateMethod.onNativeComplete(port, result);
-        var jsonStr=JSON.stringify(result);
-        alert(jsonStr);
+    };
+    for (var index in ExposeMethod) {
+        if (ExposeMethod.hasOwnProperty(index)) {
+            if (!Object.prototype.hasOwnProperty.call(RainbowBridge, index)) {
+                RainbowBridge[index] = ExposeMethod[index];
+            }
+        }
     }
-  }; 
+})();
 
-})()
 
